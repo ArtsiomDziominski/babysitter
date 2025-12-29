8e188d3f9fc2b49d7f41327233c9e5becfcb19ed
ArtsiomDziominski <artsiomdziominski@gmail.com>
2025-12-29 18:11:38 +0400

    change name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-const siteConfigFile = process.env.SITE_CONFIG || 'babysitters'
+const siteConfigFile = process.env.SITE_CONFIG || 'sitter'
 let siteConfig: any
 
 try {
